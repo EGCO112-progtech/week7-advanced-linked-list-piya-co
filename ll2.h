350b9725a625de3199d7eae7a45921574e42cd29
piya-co <piyawat.jar@student.mahidol.edu>
2025-02-17 10:57:06 +0000

    space

--- ll2.h
+++ ll2.h
@@ -143,7 +143,7 @@ void printList( LLPtr currentPtr )
 
       // while not the end of the list
       while ( currentPtr->nextPtr!= NULL ) {
-         printf("%d %s --> ", currentPtr->data, currentPtr->name);
+         printf("%d %s -->", currentPtr->data, currentPtr->name);
          currentPtr = currentPtr->nextPtr;
       } // end while
 
@@ -165,7 +165,7 @@ void printlist_reverse(LLPtr currentPtr){
       } // end while
 
       while(currentPtr!=NULL){
-      printf("%d %s --> ",currentPtr->data, currentPtr->name );
+      printf("%d %s -->",currentPtr->data, currentPtr->name );
       currentPtr=currentPtr->pPtr;
           
    } // end else
